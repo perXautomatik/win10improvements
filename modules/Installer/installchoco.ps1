51c7e8de602443df88c2974007c9e98a5e8ef0fa
TimmKroe <me@timmkroe.de>
2020-06-02 11:56:32 +0200

    fixed codestyle in installchoco

--- modules/Installer/installchoco.ps1
+++ modules/Installer/installchoco.ps1
@@ -1,3 +1,3 @@
 # Installs Chocolatey
 Write-Output "Installing Chocolatey"
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
