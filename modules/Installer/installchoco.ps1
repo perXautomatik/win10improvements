d298452ac1c6b287e6d9aed6465b5b0af5f7bc33
TimmKroe <me@timmkroe.de>
2020-06-02 14:23:19 +0200

    refactored Installation of Chocolatey script
    Signed-off-by: TimmKroe <me@timmkroe.de>

--- modules/Installer/installchoco.ps1
+++ modules/Installer/installchoco.ps1
@@ -1,3 +1,5 @@
 # Installs Chocolatey
 Write-Output "Installing Chocolatey"
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
