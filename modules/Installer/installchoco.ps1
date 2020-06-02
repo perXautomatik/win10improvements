31208f778c2f4695bec1af8353d7ffcba63b13c9
TimmKroe <me@timmkroe.de>
2020-06-02 11:53:19 +0200

    added Module Installer

---
+++ modules/Installer/installchoco.ps1
@@ -0,0 +1,3 @@
+# Installs Chocolatey
+Write-Output "Installing Chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
